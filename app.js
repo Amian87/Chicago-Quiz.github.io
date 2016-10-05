3643ec94ecc2260cd0ef6d2481e4dcc48bc25a3a
Abdulla <woodworks87@gmail.com>
2016-10-04 22:05:36 -0500

    added questions

---
+++ app.js
@@ -0,0 +1,32 @@
+var quiz = [
+	{	question: "At what street corner do all address numbers originate from in Chicago?",
+		options: ["corner of State and Madison streets", "corner of Michigan and Roosevelt", "corner of State and Congress"],
+		answer: "corner of State and Madison streets"
+	},
+	{
+		"question": "Russian president Vladimir Putin said he was excited to visit Chicago because who once lived here?",
+		"options": ["Barack Obama", "Rahm Emanuel", "Al Capone", "Oprah Winfrey"],
+		"answer": "Al Capone"
+	},
+	{
+		"question": "What is the popular name for the heart of Chicago’s downtown business district?",
+		"options": ["The Core", "The Circle", "The Loop"],
+		"answer": "The Loop"
+	},
+	{
+		"question": "What’s so special about the city’s fire hydrants?",
+		"options": ["They were the first in the country to be painted red.", "Their two-outlet design is patented and cannot be used outside city limits.", "They were specially ordered from Ohio after the Great Chicago Fire in 1871.", "An 1872 law says they must be placed every 200 feet along city blocks."],
+		"answer": "Their two-outlet design is patented and cannot be used outside city limits."
+	},
+	{
+		"question": "How many operable drawbridges are there along the Chicago River?",
+		"options": ["10", "18", "29", "37"],
+		"answer": "37"
+	},
+	{
+		"question": "Bruce Graham designed the Willis Tower and which other Chicago landmark?",
+		"options": ["John Hancock Center", "Aon Center", "Smurfit-Stone Building", "Two Prudential Plaza"],
+		"answer": "John Hancock Center"
+	},
+
+];//quiiz array
